02d94dcffa3e02eecd1b1b4dab64668258f105e8
kaoapithepp <momogg334@gmail.com>
2022-09-12 22:25:50 +0700

    refactor: move styling into page and create global components

---
+++ src/common/components/Button.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export const Button = styled.div`
+    background-color: var(--button-color) ;
+    color: white;
+    padding: 14px 0px;
+    border-radius: 10px;
+    font-size: medium;
+    text-align: center;
+    cursor: pointer;
+`;
--- src/modules/welcome/pages/Welcome.js
+++ src/modules/welcome/pages/Welcome.js
@@ -1,30 +1,86 @@
-import { Link } from "react-router-dom";
-import { Container, Header, Content, Button } from "./Welcome.styled";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import { Button } from "../../../common/components/Button";
 
 const Welcome = () => {
-  const linkStyle = {
-    textDecoration: "none",
-  };
+  const navigate = useNavigate();
+
+  function handleStartButtonClick(e) {
+    e.preventDefault();
+    navigate("/input", { replace: false });
+  }
 
   return (
     <Container>
       <Header>
         <h2>Diabetes and blood pressure Checker</h2>
-        <h3>คัดกรองโรคเบาหวานและความดันโลหิต ด้วยปิงปอง7สี</h3>
+        <h3>คัดกรองโรคเบาหวานและความดันโลหิตด้วยปิงปอง 7 สี</h3>
       </Header>
-      
       <Content>
+        <span>จุดประสงค์</span>
         <p>
-          จุดประสงค์ <br />
-          1.คัดกรองโรคเบาหวานและความดันโลหิตสูงของประชาชนที่มี อายุ  15 ปีขึ้นไป <br />
-          2.นำผลคัดกรอง/ข้อมูล จัดกลุ่มและระดับความรุนแรงของโรคด้วย "ปิงปองสีจราจรชีวิต 7 สี"การใช้ "สี" บอกระดับอาการป่วยและการปฏิบัติตัวให้ถูกหลัก3อ2ส 
+          1)  คัดกรองโรคเบาหวานและความดันโลหิตสูงของประชาชนที่มีอายุ  15 ปีขึ้นไป <br />
         </p>
-
+        <p>
+          2)  นำผลคัดกรอง/ข้อมูล จัดกลุ่มและระดับความรุนแรงของโรคด้วย "ปิงปองสีจราจรชีวิต 7 สี" การใช้สีบอกระดับอาการป่วยและการปฏิบัติตัวให้ถูกหลัก 3อ2ส
+        </p>
         <img src="/image/TaeAugust11.jpg" alt="Welcome" className="img" />
-        <Link to='/input' style={linkStyle}><Button>เริ่มทำแบบทดสอบ</Button></Link>
+        <Button onClick={handleStartButtonClick}>เริ่มทำแบบทดสอบ</Button>
       </Content>
+      <Footer>
+        © Lettuce Dog, 2022 All rights reserved.
+      </Footer>
     </Container>
   );
 }
+
+const Container =  styled.div`
+    vertical-align: middle;
+    margin-left: auto;
+    margin-right: auto;
+    width: 60vh;
+    @media screen and (max-width: 768px) {
+        width: 80%  
+    }
+`;
+
+const Header = styled.div`
+    h2 {
+        color: var(--primary-color);
+        font-weight: 500;
+        margin-bottom: 12px;
+        font-size: 32px;
+    }
+    h3 {
+        color: var(--button-color);
+        font-weight: 500;
+    }
+`;
+
+const Content = styled.div`
+    margin-top: 24px;
+    margin-bottom: 24px;
+    
+    span {
+      color: var(--white);
+      background-color: var(--button-color);
+      padding: 0px 4px;
+    }
+
+    p {
+      font-weight: 300;
+    }
+
+    .img {
+        width: 100%;
+    }
+`;
+
+const Footer = styled.div`
+  color: var(--button-color);
+  font-size: 12px;
+  text-align: center;
+`
 
 export default Welcome;
